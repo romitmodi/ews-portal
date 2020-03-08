1b41d3425f1eded24b708393024ac29705aaae1e
Romit Modi <modiromit17@gmail.com>
2020-03-08 22:16:40 +0000

    update change

--- src/main/java/com/ews/springboot/web/dao/AlertMapper.java
+++ src/main/java/com/ews/springboot/web/dao/AlertMapper.java
@@ -15,28 +15,28 @@ public interface AlertMapper {
 
 	@Select("select EWS_ID AS EWSID,Rundate,DSID,JobName,TestID,TestCaseDescription,PriorResult,MinResult,Result,MaxResult,"
 			+ "TestStatus,Classification,Measure,Summary,DetailComments,Severity,RallyDefect,ServiceNowTix,Status,Owner,"
-			+ "Resolution_PlanToClose\r\n" + " from  CommandCenter_EWS_EarlyWarningSystem\r\n"
+			+ "Resolution_PlanToClose\r\n" + " from  DS.CommandCenter_EWS_EarlyWarningSystem\r\n"
 			+ "	where rundate >= #{runDate}\r\n" + " and Classification is null\r\n"
 			+ "	order by rundate desc, jobname, testid")
 	List<Alerts> getAlertData(@Param("runDate") Date RunDate);
 
 	@Select("select EWS_ID AS EWSID,Rundate,DSID,JobName,TestID,TestCaseDescription,PriorResult,MinResult,Result,MaxResult,"
 			+ "TestStatus,Classification,Measure,Summary,DetailComments,Severity,RallyDefect,ServiceNowTix,Status,Owner,"
-			+ "Resolution_PlanToClose\r\n" + " from  CommandCenter_EWS_EarlyWarningSystem\r\n"
+			+ "Resolution_PlanToClose\r\n" + " from  DS.CommandCenter_EWS_EarlyWarningSystem\r\n"
 			+ "	where rundate >= #{runDate}\r\n" + " and Classification is null\r\n" + "AND JobName=#{jobName}"
 			+ "	order by rundate desc, jobname, testid")
 	List<Alerts> getAlertDataWithFilter(@Param("jobName") String jobName, @Param("runDate") Date RunDate);
 
 	@Select("select Jobname, JobDescription, rundate as createdate, status,  measure, summary, detailcomments, Owner,Resolution_PlanToClose,  count(*) as Teststeps  "
-			+ "from  CommandCenter_EWS_EarlyWarningSystem\r\n" + "	where status not in ('Closed')\r\n"
+			+ "from  DS.CommandCenter_EWS_EarlyWarningSystem\r\n" + "	where status not in ('Closed')\r\n"
 			+ "	group by Jobname, JobDescription, rundate, status,  measure, summary, detailcomments, Owner,Resolution_PlanToClose\r\n"
 			+ "	order by JobName")
 	List<Alerts> getAlertDataBasedOnStatus();
 
-	@Select("SELECT * FROM CommandCenter_EWS_EarlyWarningSystem WHERE EWS_ID=#{ewsId}")
+	@Select("SELECT * FROM DS.CommandCenter_EWS_EarlyWarningSystem WHERE EWS_ID=#{ewsId}")
 	Alerts getAlertDetails(@Param("ewsId") int ewsId);
 
-	@Update("update CommandCenter_EWS_EarlyWarningSystem\r\n" + "    set    Classification=#{alert.classification},\r\n"
+	@Update("update DS.CommandCenter_EWS_EarlyWarningSystem\r\n" + "    set    Classification=#{alert.classification},\r\n"
 			+ "           Measure=#{alert.measure},\r\n" + "           Summary=#{alert.summary},\r\n"
 			+ "           DetailComments=#{alert.detailComments},\r\n" + "           Severity=#{alert.severity},\r\n"
 			+ "           RallyDefect=#{alert.rallyDefect},\r\n"
